ca04ceaa413cb574e9257701b44e2dbf0d478537
whispersoftheelders <205708280+wote-dev@users.noreply.github.com>
2025-09-09 13:24:18 +1000

    fixed confirmation messages mobile

--- simplr-front-end/simplr-react/src/components/ui/toast.tsx
+++ simplr-front-end/simplr-react/src/components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed bottom-0 right-0 z-[100] flex max-h-screen w-full flex-col p-4 md:max-w-[420px]",
+      "fixed bottom-0 right-0 z-[100] flex max-h-screen w-full flex-col p-4 pb-20 sm:pb-4 md:max-w-[420px]",
       className
     )}
     {...props}
