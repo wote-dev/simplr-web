bbfa29effff5af76839ad60213b6b1229f47cf78
whispersoftheelders <205708280+wote-dev@users.noreply.github.com>
2025-09-10 16:53:48 +1000

    fix?

--- simplr-front-end/simplr-react/vite.config.ts
+++ simplr-front-end/simplr-react/vite.config.ts
@@ -23,11 +23,5 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
     sourcemap: false,
     minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
   },
 })
